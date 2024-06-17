48f8e455cdc3fccb5ce0778b00f5114370108641
Alix ANNERAUD <alix@anneraud.fr>
2024-06-17 19:17:35 -0400

    Getting started with sensors

---
+++ Sensor/src/main.cpp
@@ -0,0 +1,15 @@
+#include <Arduino.h>
+
+
+void setup() {
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
+
+// put function definitions here:
+int myFunction(int x, int y) {
+  return x + y;
+}
