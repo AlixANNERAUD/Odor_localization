91263aa075713c817aed61b07b84c2c73a93cb5e
Alix ANNERAUD <alix@anneraud.fr>
2024-07-07 18:19:34 -0400

    Add plotting to server and use more numpy array instead of list for faster computations

--- Server/main.py
+++ Server/main.py
@@ -4,6 +4,8 @@ from sensor import SensorClass
 import numpy
 import scipy 
 import json
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 
 MQTT_BROKER = "alixloicrpi.local"
 MQTT_PORT = 1883
@@ -16,7 +18,10 @@ def on_connect(client, userdata, flags, rc):
     else:
         print(f"Failed to connect to MQTT Broker, return code {rc}\n")
 
+
 def on_message(client, userdata, msg):
+    global start
+
     message = json.loads(msg.payload.decode())
 
     identifier = message["sensor"]
@@ -25,12 +30,12 @@ def on_message(client, userdata, msg):
 
     timestamp = message["timestamp"]
 
-    sensors[identifier].update(data, timestamp)
+    if start is None:
+        start = timestamp
 
-    for _, sensor in sensors.items():
-        print(f"{str(sensor)}")
-
-    print("\n")
+    for key, value in data.items():
+        if not(sensors[identifier].update(key, value, timestamp)):
+            print(f"Failed to update sensor {identifier} with data {data}")
 
 def get_sensors_position():
     sensors_positions = []
@@ -39,33 +44,95 @@ def get_sensors_position():
 
     return numpy.array(sensors_positions)
 
-def get_sensors_value():
+def get_sensors_value(gaz_sensor_type):
     sensors_values = []
     for _, sensor in sensors.items():
-        if sensor.get_value() is None:
+        sensor_values = sensor.get_latest_values(gaz_sensor_type)
+        if sensor_values is None:
             return None
-        sensors_values.append(sensor.get_value())
+        values, _ = sensor_values
+        sensors_values.append(values)
 
     return numpy.array(sensors_values)
 
 def localize_source():
     sensors_position = get_sensors_position()
-    sensors_value = get_sensors_value()
+    sensors_value = get_sensors_value("MQ3")
+   
+    print(f"Sensor value : {sensors_value}")
 
     if sensors_value is None:
+        print("Not enough sensors !")
         return None
 
-    triangulation = scipy.spatial.Delaunay(sensors_position)
+    # If we have less than 3 sensors
+    if sensors_value.shape[0] < 3:
+        x_1, y_1 = sensors_position[0]
+        x_2, y_2 = sensors_position[1]
 
-    source_coordinates = numpy.average(sensors_position[triangulation.simplices], axis=1, weights=sensors_value)
+        
 
-    return source_coordinates
+        max_value = numpy.max([sensors_value])
+
+        r_1 = 1 + max_value - sensors_value[0]
+        r_2 = 1 + max_value - sensors_value[1]
+
+        d = numpy.sqrt((x_2 - x_1)**2 + (y_2 - y_1)**2)
+
+        if d >= r_1 + r_2 or d <= numpy.abs(r_1 - r_2) or (d == 0 and r_1 != r_2):
+            print("Failed !")
+            return None
+
+        a = (r_1**2 - r_2**2 + d**2) / (2 * d)
+
+        h = numpy.sqrt(r_1**2 - a**2)
+
+        x_3 = x_1 + a * (x_2 - x_1) / d
+
+        y_3 = y_1 + a * (y_2 - y_1) / d
+
+        intersection_1 = (x_3 + h * (y_2 - y_1) / d, y_3 - h * (x_2 - x_1) / d)
+        intersection_2 = (x_3 - h * (y_2 - y_1) / d, y_3 + h * (x_2 - x_1) / d)
+
+        angle = numpy.arctan2(intersection_1[1] - y_1, intersection_1[0] - x_1)
+
+        angle_deg = numpy.degrees(angle)
+
+        return angle_deg
+    # If we have 3 sensors or more, we use delaunay triangulation
+    else:
+        triangulation = scipy.spatial.Delaunay(sensors_position)
+
+        source_coordinates = numpy.average(sensors_position[triangulation.simplices], axes=1, weights=sensors_value)
+
+        return source_coordinates
 
 sensors = {
-    "Sensor_1": SensorClass(0, 0),
-    "Sensor_2": SensorClass(0, 1),
-    "Sensor_3": SensorClass(1, 0),
+    "Sensor_1": SensorClass(23, 0),
+    "Sensor_2": SensorClass(0, 0),
 }
+
+start = None
+
+def plot_sensor_values(axes, gaz_sensor_type):
+    axes.clear()
+    axes.set_title(f"Sensors values for {gaz_sensor_type}")
+    axes.set_xlabel("Time (s)")
+    axes.set_ylabel("Value")
+
+    any_plot = False
+
+    for name, sensor in sensors.items():
+        values = sensor.get_all_values(gaz_sensor_type)
+        if values is not None:
+            axes.plot((values[0] - start)/1000, values[1], label=name)
+            any_plot = True
+
+    if any_plot:
+        axes.legend()
+        plt.plot(block=False)
+
+        plt.pause(0.1)
 
 def main():
     client = mqtt.Client()
@@ -78,21 +145,30 @@ def main():
         print("Failed to connect to MQTT Broker : ", e)
         return
 
+
     try:
         client.loop_start() 
+
+        figure = plt.figure()
+        axes = figure.add_subplot(1,1,1)
+
         while True:
             localization = localize_source()
-
+   
             if localization is None:
                 print(f"Waiting for all sensors data ...")
             else:
                 print(f"Source localized at {localization}.")
-            
-            time.sleep(1)  
+         
+            plot_sensor_values(axes, "MQ3")
+
     except KeyboardInterrupt:
         print("Disconnecting from MQTT Broker")
+
         client.loop_stop()
         client.disconnect()
+
+
 
 
 if __name__ == "__main__":
--- Server/sensor.py
+++ Server/sensor.py
@@ -1,23 +1,69 @@
+import numpy
+
 class SensorClass:
     
-    def __init__(self, x, y):
+    def __init__(self, x, y, maximum_values=10000):
         self.x = x
         self.y = y
-        self.value = None
-        self.timestamp = None
 
-    def update(self, value, timestamp):
-        self.value = value
-        self.timestamp = timestamp
+        self.a = { }
+        self.index = { }
+
+        self.maximum_values = maximum_values
+
+    
+
+    def update(self, gaz_sensors_type, value, timestamp):
+        if gaz_sensors_type not in self.a:
+            self.index[gaz_sensors_type] = 0
+            self.a[gaz_sensors_type] = numpy.zeros((2, self.maximum_values))
+
+
+
+        if self.index[gaz_sensors_type] >= self.a[gaz_sensors_type].shape[1]:
+            return False
+
+        self.a[gaz_sensors_type][:, self.index[gaz_sensors_type]] = numpy.array([timestamp, value])
+
+        self.index[gaz_sensors_type] += 1
+
+        return True
         
     def get_position(self):
         return self.x, self.y
 
-    def get_value(self):
-        return self.value
+    def get_gaz_sensors_types(self):
+        return list(self.a.keys())
 
-    def get_timestamp(self):
-        return self.timestamp
+    def get_latest_values(self, gaz_sensors_type, center=True):
+        if gaz_sensors_type not in self.a:
+            return None
+
+        i = self.index[gaz_sensors_type]
+        
+        if i == 0:
+            return None
+
+        value = self.a[gaz_sensors_type][:, i - 1].copy()
+
+        if center:
+            value[1] -= self.a[gaz_sensors_type][1, :i].mean()
+
+        return value
+
+    def get_all_values(self, gaz_sensors_type, center=True):
+        if gaz_sensors_type not in self.a:
+            return None
+
+        i = self.index[gaz_sensors_type]
+        values_copy = self.a[gaz_sensors_type].copy()
+
+        if center and i > 0:
+            values_copy[1, :i] -= values_copy[1, :i].mean()
+        
+
+        return values_copy[:, :i]
+        
 
     def __str__(self):
-        return f"Sensor ({self.x}, {self.y}) :\n - Value {self.value}\n - Last update : {self.timestamp}"
+        return f"Sensor ({self.x}, {self.y}) :\n - Value {self.get_latest_values()}"
