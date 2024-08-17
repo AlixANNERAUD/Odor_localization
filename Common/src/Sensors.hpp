1d8ecc60f492552b0c65679a85f0423158726af7
Alix ANNERAUD <alix@anneraud.fr>
2024-08-17 19:17:49 +0200

    Write documentation in common

--- Common/src/Sensors.hpp
+++ Common/src/Sensors.hpp
@@ -5,26 +5,38 @@
 
 // - Types
 
+/// @brief Sensors array type
+/// @tparam N Number of sensors
 template <size_t N>
 using SensorsType = std::array<std::tuple<const char *, MQSensorClass, float>, N>; // - Name, Sensor, Position
 
+/// @brief Sensors data type
+/// @tparam N Number of sensors
 template <size_t N>
 using SensorsDataType = std::array<std::tuple<const char *, float, uint64_t>, N>; // - Name, Value, Timestamp
 
 // - Functions
 
+/// @brief Initialize sensors
+/// @tparam N Number of sensors
+/// @param sensors Sensor data
 template <size_t N>
 inline void initialize_sensors(SensorsType<N> &sensors)
 {
     for (auto &[name, sensor, position] : sensors)
-    {
         sensor.initialize();
-    }
 }
 
+/// @brief Read sensors data
+/// @tparam N Number of sensors
+/// @param sensors Sensor data
+/// @param data Data
+/// @param get_unix_timestamp Function to get the current timestamp in milliseconds
+/// @return True if the operation was successful, false otherwise
 template <size_t N>
 inline bool read_sensors(SensorsType<N> &sensors, SensorsDataType<N> &data, std::function<bool(uint64_t &)> get_unix_timestamp)
 {
+    // - Iterate over sensors
     for (size_t i = 0; i < N; i++)
     {
         // - Sensor acquisition
@@ -45,6 +57,10 @@ inline bool read_sensors(SensorsType<N> &sensors, SensorsDataType<N> &data, std:
     return true;
 }
 
+/// @brief Get the values from the sensor data
+/// @tparam N Number of sensors
+/// @param data Sensor data
+/// @return Values
 template <size_t N>
 inline std::array<float, N> get_values(SensorsDataType<N> &data)
 {
